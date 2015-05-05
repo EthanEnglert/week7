7aa0507f933012324cab8a921ee496bbc2b7f04c
ethanenglert <ethanenglert@seattleacademy.org>
2015-05-05 14:58:31 -0700

    work on creating the prompt for the personaility quiz

--- truthordare/app.js
+++ truthordare/app.js
@@ -6,14 +6,15 @@ var options = {
     ca: fs.readFileSync('/etc/ssl/server.ca.crt')
 };
 
-var quotes = [];
-quotes.push("What personality traits would cause you to end a friendship?");
-quotes.push("Have you ever lied to your best friend?  If so, describe what happened.");
-quotes.push("How long have you gone without showering?");
+
+var truths = [];
+truths.push("Do you praise lord GabeN?");
+truths.push("Do you hate lord GabeN?");
+truths.push("Do you know lord GabeN");
 
 
-function getRandomQuote() {
-    return quotes[Math.floor(Math.random() * quotes.length)];
+function getRandomTruth() {
+    return truths[Math.floor(Math.random() * truths.length)];
 }
 
 
@@ -34,19 +35,23 @@ https.createServer(options, function(req, res) {
 
 
             echoResponse.response.outputSpeech.type = "PlainText"
-            echoResponse.response.outputSpeech.text = "Do you want truth or dare?"
+            echoResponse.response.outputSpeech.text = "Do you want truth or GabeN?"
             echoResponse.response.shouldEndSession = "false";
             theRequest = JSON.parse(jsonString);
             console.log('JSON', theRequest.request);
             if (typeof theRequest.request.intent !== 'undefined') {
                 choice = theRequest.request.intent.slots.Choice.value;
-                echoResponse.response.outputSpeech.text = "you said " + choice;
+                if (choice === "truth"){
+                truth = getRandomTruth();
+                echoResponse.response.outputSpeech.text = truth
+                //echoResponse.response.outputSpeech.text = "you said " + choice;
                 // echoResponse.response.card = {}
                 // echoResponse.response.card.type = "PlainText";
                 // echoResponse.response.card.title = choice;
                 // echoResponse.response.card.subtitle = choice;
                 // echoResponse.response.card.content = choice;
                 echoResponse.response.shouldEndSession = "true";
+                }
 
             }
             myResponse = JSON.stringify(echoResponse);
@@ -62,4 +67,4 @@ https.createServer(options, function(req, res) {
         res.writeHead(200);
         res.end(myResponse);
     }
-}).listen(xxxx); //Put number in the 3000 range for testing and 443 for production
+}).listen(3015); //Put number in the 3000 range for testing and 443 for production
